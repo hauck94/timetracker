02c6e2592d15a1779b0ca14bc378e9885039c968
Marcel Hauck <marcel.hauck@stud.h-da.de>
2020-11-20 00:55:14 +0100

    removed playground files

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,22 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./components/GlobalStyle";
 import { Layout } from "./components/Layout";
-import { Message, MessageType } from "./components/Message";
 import { DashboardPage } from "./pages/Dashboard/DashboardPage";
 import "./styles.css";
 import { theme } from "./theme";
-
-export interface JokeResponse {
-  type: string;
-  value: Value;
-}
-
-export interface Value {
-  id: number;
-  joke: string;
-  categories: any[];
-}
 
 export const App = () => {
   return (
