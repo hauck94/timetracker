7df2ed20b868cff18a5ba82faf0548c2f71d3b37
Marcel Hauck <marcel.hauck@stud.h-da.de>
2020-11-21 03:37:39 +0100

    proxy for backend

--- packages/backend/src/router/global.router.ts
+++ packages/backend/src/router/global.router.ts
@@ -1,9 +1,13 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { taskRouter } from "./task.router";
 import { labelRouter } from "./label.router";
 import { trackingRouter } from "./tracking.router";
 
 export const globalRouter = Router({ mergeParams: true });
+
+globalRouter.get("/", async (_: Request, res: Response) => {
+  res.send({ message: "hi from the api" });
+});
 
 // mergeParams true damit die parameter übergeben werden
 // leitet zu task Router weiter
--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./components/GlobalStyle";
 import { Layout } from "./components/Layout";
@@ -7,14 +7,20 @@ import "./styles.css";
 import { theme } from "./theme";
 
 export const App = () => {
+  useEffect(() => {
+    (async function () {
+      const helloRequest = await fetch("/api");
+      const helloJson = await helloRequest.json();
+      console.log(helloJson);
+    })();
+  });
+
   return (
-    <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Layout>
           <DashboardPage />
         </Layout>
       </ThemeProvider>
-    </>
   );
 };
