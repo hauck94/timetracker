313fa60bafd16d2100a7ca2ed66e9dbfdbdd3ac6
Marcel Hauck <marcel.hauck@stud.h-da.de>
2020-11-21 01:39:59 +0100

    Dashboard Page

--- packages/frontend/src/pages/Dashboard/DashboardPage.tsx
+++ packages/frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -1,6 +1,74 @@
 import React from "react";
 import styled from "styled-components/macro";
+import {
+  Label,
+  Task,
+  TaskItem,
+  TaskList,
+  Tracking,
+} from "./components/TaskList";
+
+const Labels: Label[] = [
+  {
+    id: 1,
+    created: new Date(),
+    name: "Uni",
+    tasks: [],
+    updatedAt: new Date(),
+  },
+  {
+    id: 2,
+    created: new Date(),
+    name: "Arbeit",
+    tasks: [],
+    updatedAt: new Date(),
+  },
+];
+
+const tasks: Task[] = [
+  {
+    created: new Date(),
+    description: "Baue eine Website mittels Angular im Frontend",
+    name: "Angular Website",
+    id: 2,
+    labels: Labels,
+    trackings: [],
+    updatedAt: new Date(),
+  },
+  {
+    created: new Date(),
+    description: "Baue eine Website mittels React im Frontend",
+    name: "Fortgeschrittene Webentwicklung",
+    id: 1,
+    labels: [],
+    trackings: [],
+    updatedAt: new Date(),
+  },
+];
+
+const trackings: Tracking[] = [
+  {
+    created: new Date(),
+    description: "test",
+    endTime: "",
+    id: 1,
+    name: "test Tracking",
+    startTime: "",
+    task: tasks[0],
+    updatedAt: new Date(),
+  },
+];
 
 export const DashboardPage = () => {
-  return <h1>Dashborad</h1>;
+  return (
+    <>
+      <h1>Dashborad FWE</h1>
+
+      <TaskList>
+        {tasks.map((task) => (
+          <TaskItem task={task}></TaskItem>
+        ))}
+      </TaskList>
+    </>
+  );
 };
---
+++ packages/frontend/src/pages/Dashboard/components/TaskList.tsx
@@ -0,0 +1,160 @@
+import React from "react";
+import styled from "styled-components";
+
+export type Task = {
+    id: number;
+    name: string;
+    description: string;
+    created: Date;
+    updatedAt: Date;
+    trackings: Tracking[];
+    labels: Label[];
+  };
+  
+  export type Label = {
+    id: number;
+    name: string;
+    created: Date;
+    updatedAt: Date;
+    tasks: Task[];
+  };
+  
+  export type Tracking = {
+    id: number;
+    name: string;
+    description: string;
+    startTime: string;
+    endTime: string;
+    created: Date;
+    updatedAt: Date;
+    task: Task;
+  };
+
+const LabelList = styled.ul`
+  list-style: none;
+  flex-grow: 1;
+  font-size: 0.8rem;
+
+  align-self: flex-end;
+  display: flex;
+  & > li {
+    margin-right: 0.5rem;
+    padding: 0.125rem;
+    border-radius: 0.25rem;
+    background-color: ${(props) => props.theme.colors.primary}
+    display: block;
+    color: #333;
+  }
+`;
+
+const TrackingList = styled.ul`
+  list-style: none;
+  flex-grow: 1;
+  font-size: 0.8rem;s
+
+  align-self: flex-end;
+  display: flex;
+  & > li {
+    margin-right: 0.5rem;
+    padding: 0.125rem;
+    border-radius: 0.25rem;
+    background-color: ${(props) => props.theme.colors.primary}
+    display: block;
+    color: #333;
+  }
+`;
+
+const TaskFlex = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const TaskHighlight = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  display: none;
+  width: 4px;
+  background-color: ${(props) => props.theme.colors.primary};
+`;
+
+export const TaskItemStyle = styled.div`
+  margin: 0;
+  min-height: 3rem;
+  position: relative;
+  padding: 0.7rem 2rem;
+  &:hover {
+    ${TaskHighlight} {
+      display: block;
+    }
+  }
+`;
+export const TaskList = styled.ul`
+  list-style: none;
+  box-shadow: 0 0.125em 0.25em 0 ${(props) => props.theme.colors.shadowColor};
+  width: 100%;
+  padding: 0;
+  border-radius: 0.5rem;
+  background-color: ${(props) => props.theme.colors.listBackgroundColor};
+  ${TaskItemStyle} {
+    border-bottom: 1px ${(props) => props.theme.colors.shadowColor} solid;
+    &:last-of-type {
+      border-bottom: 0;
+    }
+  }
+`;
+
+export const TaskTitle = styled.p`
+  font-size: 1.1rem;
+  font-weight: 500;
+  margin: 0;
+`;
+
+export const TaskDescription = styled.p`
+  font-size: 0.8rem;
+  margin: 0;
+`;
+export const TaskDate = styled.p`
+  margin: 0;
+  font-size: 0.8rem;
+  color: ${(props) => props.theme.colors.secondaryFontColor};
+`;
+export const TaskValue = styled.span`
+  white-space: nowrap;
+`;
+export type TaskItemProps = {
+  task: Task;
+};
+
+export const TaskItem: React.FC<TaskItemProps> = ({
+  task: { name, description, created, labels, trackings },
+}) => {
+  return (
+    <TaskItemStyle>
+      <TaskHighlight />
+      <TaskFlex>
+        <div>
+          <TaskTitle>{name}</TaskTitle>
+          <TaskDescription>{description}</TaskDescription>
+          <TaskDate>
+            {created && created.toLocaleString()}
+          </TaskDate>
+        </div>
+        <LabelList>
+          {labels &&
+            labels.map((label: Label) => {
+              return <li key={label.name}>{label.tasks}</li>;
+            })}
+        </LabelList>
+
+            <TrackingList>
+            {trackings &&
+            trackings.map((tracking: Tracking) => {
+              return <li key={tracking.name}>{tracking.task}</li>;
+            })}
+            </TrackingList>
+      </TaskFlex>
+    </TaskItemStyle>
+  );
+};
